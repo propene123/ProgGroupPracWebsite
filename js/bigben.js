5ad494474d9ef31659da179f220a438f551fcdfd
Ben Beddows <benjamin.e.beddows@durham.ac.uk>
2018-11-08 10:52:39 +0000

    Done the contender animations, with coloured squares.

--- js/bigben.js
+++ js/bigben.js
@@ -10,16 +10,54 @@ const contenderSpeedsInKilometersPerHour = [
 	distanceInKilometers/contenderTimesInHours[2]
 ];
 
+var currentDistances = [0.0, 0.0, 0.0];
+var contenderDone = [false, false, false];
+var contenderProgress = [0.0, 0.0, 0.0];
+
+// Where does the race start, and what is the relative vector to the end position?
+var startPos = {left: 0, top: 0};
+var dPos = {left: 0, top: 0};
+
+// Advance the distance by one second.
+function advanceDistance() {
+	for (var i = 0; i < currentDistances.length; i++) {
+		if (!contenderDone[i]) {
+			currentDistances[i] = currentDistances[i] + contenderSpeedsInKilometersPerHour[i] / 3600;
+		
+			if (currentDistances[i] > distanceInKilometers) {
+				contenderDone[i] = true;
+				contenderProgress[i] = 1.0;
+			}
+			else {
+				contenderProgress[i] = currentDistances[i] / distanceInKilometers;
+			}
+		}
+	}
+}
+
+// Advance the race by one second.
+function advanceRace() {
+	
+	advanceDistance();
+
+	// Calculate the positions for each contender.
+	for (var i = 0; i < contenderProgress.length; i++) {
+		$(".contender").eq(i).css("left", startPos.left + contenderProgress[i] * dPos.left);
+		$(".contender").eq(i).css("top", startPos.top + contenderProgress[i] * dPos.top);
+	}
+}
 
 // Called when the page loads in.
 $(function(){
+
+	$("#bb-countdown").hide();
+	$(".contender").hide();
 
 	// Called when the race begins.
 	$("#race-begin").click(function(){
 
 		// A "lock" to make sure the user doesn't press the button twice.
-		if (!raceBegun)
-		{
+		if (!raceBegun) {
 			raceBegun = true;
 
 			var checkboxes = $(".ui.radio.checkbox");
@@ -34,6 +72,33 @@ $(function(){
 				userBet++;
 			});
 
+			// Setup the bb-countdown element to be in the middle of the map.
+			var mapPosition = $("#bb-map").position();
+			var mapWidth = $("#bb-map").width();
+			var mapHeight = $("#bb-map").height();
+			var countdownWidth = $("#bb-countdown").width();
+			var countdownHeight = $("#bb-countdown").height();
+
+			// Set the race starting position and relative vector.
+			startPos.left = mapPosition.left + mapWidth * 0.67;
+			startPos.top = mapPosition.top + mapHeight * 0.22;
+
+			var endPos = {left: 0, top: 0};
+
+			endPos.left = mapPosition.left + mapWidth * 0.15;
+			endPos.top = mapPosition.top + mapHeight * 0.29;
+
+			dPos.left = endPos.left - startPos.left;
+			dPos.top = endPos.top - startPos.top;
+
+			$(".contender").css("left", startPos.left).show();
+			$(".contender").css("top", startPos.top).show();
+
+			$("#bb-countdown").css("left", mapPosition.left + (mapWidth - countdownWidth) / 2);
+			$("#bb-countdown").css("top", mapPosition.top + (mapHeight - countdownHeight) / 2);
+
+			$("#header-above-map").html("The race is starting!");
+
 			// Take the intro content and animate it away!
 			$("#bb-content-1-intro").animate({
 				height: "hide"
@@ -43,6 +108,26 @@ $(function(){
 			$([document.documentElement, document.body]).animate({
 				scrollTop: $("#header-above-map").offset().top
 			}, 1000);
+
+			// Animate the countdown.
+			$("#bb-countdown").delay(1000).fadeIn(900).delay(1000).fadeOut(100).queue(function(){
+				$(this).html("2...");
+				$(this).dequeue();
+			}).fadeIn(900).delay(1000).fadeOut(100).queue(function(){
+				$(this).html("1...");
+				$(this).dequeue();
+			}).fadeIn(900).delay(1000).fadeOut(100).queue(function(){
+				$(this).html("Go!");
+				$(this).dequeue();
+			}).fadeIn(500).fadeOut(500, function(){
+
+				// Called when the countdown animation is complete.
+				$("#header-above-map").html("And we're off! Wait... the cable's already finished???");
+
+				// Start the race immediately, and then advance the race every second.
+				advanceRace();
+				setInterval(advanceRace, 1000);
+			});
 		}
 	});
 
